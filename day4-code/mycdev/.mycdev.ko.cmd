fafadb497136f56790de10ecce1b296aa60ef6dd
dzs <zhansheng_hello@163.com>
2020-07-28 14:25:38 +0800

    day4-6

---
+++ day4-code/mycdev/.mycdev.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/linux/Linux-driver-31/day4-code/mycdev/mycdev.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.5.0-23-generic/scripts/module-common.lds --build-id  -o /home/linux/Linux-driver-31/day4-code/mycdev/mycdev.ko /home/linux/Linux-driver-31/day4-code/mycdev/mycdev.o /home/linux/Linux-driver-31/day4-code/mycdev/mycdev.mod.o
---
+++ day4-code/mycdev/.tmp_versions/mycdev.mod
@@ -0,0 +1,2 @@
+/home/linux/Linux-driver-31/day4-code/mycdev/mycdev.ko
+/home/linux/Linux-driver-31/day4-code/mycdev/mycdev.o
