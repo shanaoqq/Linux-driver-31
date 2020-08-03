8a40fa097404e226bd9830a989e5a8399b3f4f3d
dzs <zhansheng_hello@163.com>
2020-08-03 15:18:45 +0800

    day8-6

--- day8-code/platform/pdrv/pdrv.c
+++ day8-code/platform/pdrv/pdrv.c
@@ -2,10 +2,29 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 
-
+struct resource *res[2];
+unsigned int type[] = {IORESOURCE_MEM,IORESOURCE_IRQ};
+int irqno;
 int pdrv_probe(struct platform_device *pdev)
 {
+	int i;
 	printk("%s:%s:%d\n",__FILE__,__func__,__LINE__);
+	for(i=0;i<ARRAY_SIZE(res);i++){
+		res[i] = platform_get_resource(pdev,type[i],0);
+		if(res[i] == NULL){
+			printk("platform get resource[%d] error\n",i);
+			return -EAGAIN;
+		}
+	}
+
+	printk("addr = %#x,irqno = %d\n",res[0]->start,res[1]->start);
+
+	irqno = platform_get_irq(pdev,0);
+	if(irqno < 0){
+		printk("platform get irq error\n");
+		return irqno;
+	}
+	printk("irqno = %d\n",irqno);
 	return 0;
 }
 int pdrv_remove(struct platform_device *pdev)
