d097649def7bca303f231e97c7dd28392fe4517b
dzs <zhansheng_hello@163.com>
2020-08-03 14:04:53 +0800

    day8-3

---
+++ day8-code/platform/pdev/.pdev.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/linux/Linux-driver-31/day8-code/platform/pdev/pdev.ko := arm-none-linux-gnueabi-ld -EL -r  -T /home/linux/kernel/kernel-3.4.39/scripts/module-common.lds --build-id  -o /home/linux/Linux-driver-31/day8-code/platform/pdev/pdev.ko /home/linux/Linux-driver-31/day8-code/platform/pdev/pdev.o /home/linux/Linux-driver-31/day8-code/platform/pdev/pdev.mod.o
