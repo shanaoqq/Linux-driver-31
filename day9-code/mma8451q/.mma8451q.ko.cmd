ee0c55a3769d0266371b72d9d897c7d6291b0a79
dzs <zhansheng_hello@163.com>
2020-08-04 12:01:23 +0800

    day9-2

---
+++ day9-code/mma8451q/.mma8451q.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/linux/Linux-driver-31/day9-code/mma8451q/mma8451q.ko := arm-none-linux-gnueabi-ld -EL -r  -T /home/linux/kernel/kernel-3.4.39/scripts/module-common.lds --build-id  -o /home/linux/Linux-driver-31/day9-code/mma8451q/mma8451q.ko /home/linux/Linux-driver-31/day9-code/mma8451q/mma8451q.o /home/linux/Linux-driver-31/day9-code/mma8451q/mma8451q.mod.o
---
+++ day9-code/mma8451q/.tmp_versions/mma8451q.mod
@@ -0,0 +1,2 @@
+/home/linux/Linux-driver-31/day9-code/mma8451q/mma8451q.ko
+/home/linux/Linux-driver-31/day9-code/mma8451q/mma8451q.o
